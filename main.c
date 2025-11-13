d99b0d14dd5016a07503a9769c90ca45433b1eee
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-11-13 15:25:50 +0900

    실습 4

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *x, int *y){
-		 int temp;
-		 temp = *x;
-		 *x = *y;
-		 *y = temp;
-}
-
 int main(int argc, char *argv[])
 {
- 		int a = 3;
- 		int b = 5;
- 		swap(&a,&b);
- 		printf("a:%i, b:%i\n", a, b);
+ 		int i;
+ 		int grade[5];
+ 		
+ 		for(i=0; i<5; i++){
+		 printf("input value[%i] = ", i);
+		 scanf("%d", &grade[i]);
+		 }
+		 
+		 int count;
+		 count = 0;
+		 
+		 for(i=0; i<5; i++){
+			printf("grade[%i] = %i\n", i, *(grade+i));
+			count = count + *(grade+i);
+			}
+		 
+		 printf("\naverage : %i\n", count/i);
+		 
  		
   system("PAUSE");	
   return 0;
